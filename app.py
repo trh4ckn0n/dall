59041ad95537d06501fa5b55ad252cbcc2c01425
trh4ckn0n <trhacknon@proton.me>
2025-03-23 04:43:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if st.button("🚀 Générer l'Avatar"):
     mask = mask.resize(mask_size)
 
     # Positionner le masque sur l'image
-    mask_position = (img.width // 18, img.height // 2)  # Positionner à une position centrale
+    mask_position = (img.width // 22, img.height // 2)  # Positionner à une position centrale
     img.paste(mask, mask_position, mask)  # Appliquer le masque avec transparence
 
     # Afficher l'image modifiée
